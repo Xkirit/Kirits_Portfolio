6d73eaca024eaf222593e634d0ec4a3d9bf7a96c
Kirit <kiritrajut@gmail.com>
2025-04-16 19:47:12 +0530

    touches

--- src/Image.jsx
+++ src/Image.jsx
@@ -2,15 +2,15 @@ import image from './assets/pfp2.png'
 
 export default function Image(){
     return(
-        <div className="wrapper-img flex justify-center items-center overflow-visible ">
+        <div className="wrapper-img mb-10 flex justify-center items-start overflow-visible ">
           {/* <div className="box"></div> */}
           <div>
           {/* <img className="image" src={image} alt="profile portrait" loading="eager" /> */}
           
-          <div className='w-full flex justify-center items-center'>
-          <div className="w-1/2 flex right-10 md:w-1/2  md:left-10 lg:w-[250px] items-center justify-center">
+          <div className='w-full flex justify-center items-start'>
+          <div className="w-1/2 flex lg:mb-10 right-10 md:w-1/2  md:left-10 lg:w-[275px] items-center justify-center">
             <img 
-              className="image flex object-cover w-full justify-center items-center rounded-lg" 
+              className="image mb-10 flex object-cover w-full justify-center items-start rounded-lg" 
               src={image} 
               alt="profile portrait" 
               loading="eager" 
