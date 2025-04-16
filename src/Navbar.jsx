211ac132ca4236f214ab377a3d9d78c8b653a7e7
Kirit <kiritrajut@gmail.com>
2025-04-16 18:35:35 +0530

    logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
           <img 
             src={logo} 
             alt="Kirit Raju" 
-            className="w-auto max-h-10 sm:h-10 md:h-12 max-w-[80px] sm:max-w-[100px] md:max-w-[120px] object-contain" 
+            className="w-auto max-h-10 sm:h-10 md:h-12 max-w-[120px] sm:max-w-[100px] md:max-w-[120px] object-contain" 
             style={{ opacity: 1 }}
           />
         </a>
